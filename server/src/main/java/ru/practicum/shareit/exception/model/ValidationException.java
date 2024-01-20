8facdb2472f3941254f3e8b356b2dcac8ceab960
Anastasia <zharkovaanasta@yandex.ru>
2024-01-20 19:09:29 +0300

    fix tests coverage

--- server/src/main/java/ru/practicum/shareit/exception/model/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception.model;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
